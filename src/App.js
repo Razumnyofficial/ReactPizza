a5b7493cebeca8fd3b053848c51d2088cec2a5c4
Valentin <razumny.valentin@gmail.com>
2024-11-08 14:42:09 +0300

    start new coding

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Sort from "./components/Sort";
+import Categories from "./components/Categories";
+import PizzaBlock from "./components/PizzaBlock";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Пепперони Фреш" price={350} />
+            <PizzaBlock title="Чизбургер" price={400} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {
+          categories.map((category, index) => (
+            <li
+              key={index}
+              onClick={() => onClickCategory(index)}
+              className={activeIndex === index ? "active" : ""}
+            >
+              {category}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
